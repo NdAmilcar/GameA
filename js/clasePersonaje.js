fe1c9bedc40571e2aab702c0df624db9bf65e99b
Amilcar <86882319+NdAmilcar@users.noreply.github.com>
2022-02-09 22:35:33 -0300

    Update clasePersonaje.js

--- js/clasePersonaje.js
+++ js/clasePersonaje.js
@@ -11,9 +11,9 @@ export class Ahorcado {
     cambiarFondo(){
         let contenedor = document.querySelector("body");
         if(this.vivo != false){
-        contenedor.style.backgroundImage = "url(../imgDemon/cabania.gif)";
+        contenedor.src = "url(../imgDemon/cabania.gif)";
         }else if(this.vivo == false){
-            contenedor.style.backgroundImage = "url(../imgDemon/fire2.gif)";
+            contenedor.src = "url(../imgDemon/fire2.gif)";
         }
     }
     //---------------------------------------------------------------------------------------------------------------------
